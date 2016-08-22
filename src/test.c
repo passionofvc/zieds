a945fa7746832cbbe84ff59d5357e97b36138cf3
Grif Hughes <ghughes@smu.edu>
2016-08-22 00:37:16 -0500

    Edited exmaple program to use strings

--- src/test.c
+++ src/test.c
@@ -6,12 +6,12 @@ int main(void)
         //initialize
         struct bloom_filter *bf = bloom_init();
 
-        //add some numbers
+        //add some strings 
         bloom_insert_string(bf, "Hello");
         bloom_insert_string(bf, "Hey");
         bloom_insert_string(bf, "Sup");
 
-        //prints 1 0 1
+        //prints 0 0 1
         printf("%d %d %d\n", bloom_query_string(bf, "Hi"), 
         bloom_query_string(bf, "Yo"),
         bloom_query_string(bf, "Hey")); 
