0d7caafb1a31f09393a99fee6ad2b7cc50528a60
Grif Hughes <ghughes@smu.edu>
2016-08-23 08:30:38 -0500

    Fixed src file

--- src/bloom_filter.c
+++ src/bloom_filter.c
@@ -18,36 +18,42 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
 #include "../include/bloom_filter.h"
 #include "hashes.h"
 
 #define SET_BIT(bf, k) (bf->buf[k >> 4] |= 1 << (k % 16))
 #define TEST_BIT(bf, k) (bf->buf[k >> 4] & 1 << (k % 16))
 
-#define ALLOC_ERR 43 
-
 #define H_a(pointer, bytes, out) (MurmurHash3_x86_32((void *)pointer, bytes, 0, out))
 #define H_b(pointer, bytes) (FNV1A_Hash_WHIZ((void *)pointer, bytes))
 #define G(in, scalar, pointer, bytes, size) (abs(in[0] + scalar * H_b(pointer, bytes)) % size)
 
+void bloom_insert_int(struct bloom_filter *bf, const int32_t data)
+{
+        uint8_t i;
+        uint32_t hash[4];
+
+        H_a(&data, sizeof(data), hash);
+        for(i = 0; i < bf->nhashes; ++i) 
+                SET_BIT(bf, G(hash, i, &data, sizeof(data), bf->mbits));
+}
 void bloom_insert_string(struct bloom_filter *bf, const char *data)
 {
+        uint8_t i;
         uint32_t hash[4];
         uint64_t len = strlen(data);
-        
+
         H_a(data, len, hash);
-        uint8_t i;
         for(i = 0; i < bf->nhashes; ++i)
                 SET_BIT(bf, G(hash, i, data, len, bf->mbits));
 }
 
 bool bloom_query_int(struct bloom_filter *bf, const int32_t data)
 {
+        uint8_t i;
         uint32_t hash[4];
 
         H_a(&data, sizeof(data), hash);
-        uint8_t i;
         for(i = 0; i < bf->nhashes; ++i) 
                 if(!TEST_BIT(bf, G(hash, i, &data, sizeof(data), bf->mbits)))
                         return false;
@@ -56,11 +62,11 @@ bool bloom_query_int(struct bloom_filter *bf, const int32_t data)
 
 bool bloom_query_string(struct bloom_filter *bf, const char *data)
 {
+        uint8_t i;
         uint32_t hash[4];
         uint64_t len = strlen(data);
 
         H_a(data, len, hash);
-        uint8_t i;
         for(i = 0; i < bf->nhashes; ++i) 
                 if(!TEST_BIT(bf, G(hash, i, data, len, bf->mbits)))
                         return false;
